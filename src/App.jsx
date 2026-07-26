9e323391e2465c3f678415681712f27f6d584272
Dominic-SR <dominicseduraja3@gmail.com>
2026-07-26 23:10:38 +0530

    folder setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { useState,useEffect } from "react";
 import './App.css'
-
+import AppRoutes from "./routes/AppRoutes"
 function App() {
-  return (
-    <div>
-
-    </div>
-  )
+  return (<AppRoutes />)
 }
 
 export default App
